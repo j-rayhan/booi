ee328cd8a28ad4b50dfcff0b01e43b145edd421f
johir-rayhan <johir.rayhan01@gmail.com>
2021-10-31 14:55:15 +0600

    Login form done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import type { NextPage } from 'next'
 import Image from 'next/image'
-import { faHome, faList, faTags, faComment, faBlog, faSearch, faBook, faHeart, faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faList, faTimes, faTags, faComments, faBlog, faSearch, faBook, faHeart, faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   const [active, setActive] = React.useState<boolean>(false);
   const [searchFormVisible, setSearchFormVisible] = React.useState<boolean>(false);
+  const [loginFormVisible, setLoginFormVisible] = React.useState<boolean>(false);
   const toggleActive = () => {
     const scrolled = document.documentElement.scrollTop;
     if (scrolled > 80) {
@@ -21,7 +22,7 @@ const Home: NextPage = () => {
     return () => subscribe;
   })
   const handleSearchFormVisible = () => setSearchFormVisible(!searchFormVisible);
-
+  const handleLoginFormVisible = () => setLoginFormVisible(!loginFormVisible)
   return (
     <div className={styles.container}>
       {/* Header section */}
@@ -39,7 +40,9 @@ const Home: NextPage = () => {
             <div id="search-btn" className={styles.searchBtn} onClick={() => handleSearchFormVisible()}><FontAwesomeIcon icon={faSearch} /></div>
             <a href="#heart"><FontAwesomeIcon icon={faHeart} /></a>
             <a href="#shoppingCart"><FontAwesomeIcon icon={faShoppingCart} /></a>
-            <div id="login-btn"><FontAwesomeIcon icon={faUser} /></div>
+            <div id="login-btn" onClick={() => handleLoginFormVisible()}>
+              <FontAwesomeIcon icon={faUser} />
+            </div>
           </div>
 
         </div>
@@ -55,15 +58,44 @@ const Home: NextPage = () => {
         </div>
 
       </header>
-      {/* <!-- bottom navbar  --> */}
-
+      {/* bottom navbar */}
       <nav className={styles.bottomNavbar}>
         <a href="#home"><FontAwesomeIcon icon={faHome} /></a>
         <a href="#featured" ><FontAwesomeIcon icon={faList} /></a>
         <a href="#arrivals" ><FontAwesomeIcon icon={faTags} /></a>
-        <a href="#reviews"><FontAwesomeIcon icon={faComment} /></a>
+        <a href="#reviews"><FontAwesomeIcon icon={faComments} /></a>
         <a href="#blogs" ><FontAwesomeIcon icon={faBlog} /></a>
       </nav>
+
+      
+{/* login form */}
+
+<div className={`${styles.loginFormContainer} ${loginFormVisible ? styles.active : null}`}>
+
+    <div
+      className={styles["close-login-btn"]}
+      onClick={() => handleLoginFormVisible()}
+    >
+      <FontAwesomeIcon icon={faTimes} />
+    </div>
+
+    <form action="">
+        <h3>sign in</h3>
+        <span>username</span>
+        <input type="email" name="" className={styles.box} placeholder="enter your email" id="" />
+        <span>password</span>
+        <input type="password" name="" className={styles.box} placeholder="enter your password" id="" />
+        <div className={styles.checkbox}>
+            <input type="checkbox" name="" id="remember-me" />
+            <label htmlFor="remember-me"> remember me</label>
+        </div>
+        <input type="submit" value="sign in" className={styles.btn} />
+        <p>forget password ? <a href="#">click here</a></p>
+        <p>don&apos;t have an account ? <a href="#">create one</a></p>
+    </form>
+
+</div>
+
     </div>
   )
 }
