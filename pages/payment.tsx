2168e91fb2735113c2b240563f2646ae27923420
johir-rayhan <johir.rayhan01@gmail.com>
2021-12-19 09:08:33 +0600

    Start payment gateway

---
+++ pages/payment.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import type { NextPage } from 'next'
+
+const Payment: NextPage = () => {
+  return (
+    <div>
+      <h1>Payment form</h1>
+    </div>
+  )
+}
+
+export default Payment;
